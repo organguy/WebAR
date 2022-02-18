7788803bf5d582c8020bc92e3b1d11fe15ec2189
organguy <organguy@naver.com>
2022-02-19 03:52:51 +0900

    - autostart

--- lectures-start/232-fix-autostart/main.js
+++ lectures-start/232-fix-autostart/main.js
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
     const {renderer, scene, camera} = mindarThree;
 
-
+    const video = await loadVideo("../../assets/videos/sintel/sintel.mp4");
     const texture = new THREE.VideoTexture(video);
 
     const geometry = new THREE.PlaneGeometry(1, 204/480);
