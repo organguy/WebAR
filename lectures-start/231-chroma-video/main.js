cc3e6b6a3975baa4604dd9cbea3528afa0fe8f06
organguy <organguy@naver.com>
2022-02-19 03:35:08 +0900

    - chroma

--- lectures-start/231-chroma-video/main.js
+++ lectures-start/231-chroma-video/main.js
@@ -1,11 +1,12 @@
 import {loadGLTF, loadVideo} from "../../libs/loader.js";
 import {mockWithVideo} from '../../libs/camera-mock';
+import {createChromaMaterial} from '../../libs/chroma-video.js';
 const THREE = window.MINDAR.IMAGE.THREE;
 
 document.addEventListener('DOMContentLoaded', () => {
   const start = async() => {
     mockWithVideo('../../assets/mock-videos/course-banner1.mp4');
-    
+
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
       imageTargetSrc: '../../assets/targets/course-banner.mind',
@@ -16,8 +17,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const texture = new THREE.VideoTexture(video);
 
     const geometry = new THREE.PlaneGeometry(1, 1080/1920);
-    const material = new THREE.MeshBasicMaterial({map: texture});
-    
+    //const material = new THREE.MeshBasicMaterial({map: texture});
+    const material = createChromaMaterial(texture, 0x00ff00);
+    const plane = new THREE.Mesh(geometry, material);
+
+    plane.rotation.x = Math.PI / 2;
+    plane.position.y = 0.7;
+    plane.scale.multiplyScalar(4);
+
     const anchor = mindarThree.addAnchor(0);
     anchor.group.add(plane);
 
