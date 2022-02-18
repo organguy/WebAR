d6feef1341e3fabdf178ec949c3b2635784999e3
organguy <organguy@naver.com>
2022-02-19 04:05:54 +0900

    - CSS

--- lectures-start/240-css-renderer/main.js
+++ lectures-start/240-css-renderer/main.js
@@ -1,3 +1,4 @@
+import {(CSS3DObject)} from "../../libs/three.js-r132/examples/jsm/renderers/CSS3DRenderer.js";
 const THREE = window.MINDAR.IMAGE.THREE;
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -6,11 +7,15 @@ document.addEventListener('DOMContentLoaded', () => {
       container: document.body,
       imageTargetSrc: '../../assets/targets/course-banner.mind',
     });
-    const {renderer, scene, camera} = mindarThree;
+    const {renderer, cssRenderer, scene, cssScene, camera} = mindarThree;
+
+    const obj = new CSS3DObject(document.querySelector("#ar-div"));
+    const cssAnchor = mindarThree.addCSSAnchor(0);
+    cssAnchor.group.add(obj);
 
     await mindarThree.start();
     renderer.setAnimationLoop(() => {
-      renderer.render(scene, camera);
+      cssRenderer.render(cssScene, camera);
     });
   }
   start();
