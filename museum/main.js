1b9d59d6e57aeac9c57a113a49e03ff05b9ed01f
organguy <organguy@naver.com>
2022-02-20 19:08:56 +0900

    edt

--- museum/main.js
+++ museum/main.js
@@ -19,12 +19,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
     scene.add(light);
 
-    const model = await loadGLTF("./model/ps01001002_03.glb");
+    //const model = await loadGLTF("./model/ps01001002_03.glb");
     //const model = await loadGLTF("./model/ps01001005_10.glb");
     //const model = await loadGLTF("./model/ps01001008_07.glb");
     //const model = await loadGLTF("./model/ps01001010_02.glb");
     //const model = await loadGLTF("./model/ps01001011_07.glb");
-    //const model = await loadGLTF("../assets/models/musicband-raccoon/scene.gltf");
+    const model = await loadGLTF("../assets/models/musicband-raccoon/scene.gltf");
     model.scene.scale.set(scaleVal, scaleVal, scaleVal);
     model.scene.position.set(0, -0.4, 0);
 
