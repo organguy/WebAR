2d807998a3adee01c739e27283d61a694b1d407d
organguy <organguy@naver.com>
2022-02-20 14:27:35 +0900

    edt

--- museum/main.js
+++ museum/main.js
@@ -16,7 +16,8 @@ document.addEventListener('DOMContentLoaded', () => {
     scene.add(light);
 
     //const model = await loadGLTF("./model/ps01001002_03.glb");
-    const model = await loadGLTF("./model/ps01001005_10.glb");
+    //const model = await loadGLTF("./model/ps01001005_10.glb");
+    const model = await loadGLTF("./model/ps01001008_07.glb");
     //const model = await loadGLTF("../assets/models/musicband-raccoon/scene.gltf");
     model.scene.scale.set(scaleVal, scaleVal, scaleVal);
     model.scene.position.set(0, -0.4, 0);
