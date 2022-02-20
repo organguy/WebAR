4160203bdc3476989b0dad7146e64f59af524d0e
organguy <organguy@naver.com>
2022-02-20 12:20:13 +0900

    - nm edit

--- museum/main.js
+++ museum/main.js
@@ -5,14 +5,16 @@ document.addEventListener('DOMContentLoaded', () => {
   const start = async() => {
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
-      imageTargetSrc: './target/ps01001002_03.mind',
+      //imageTargetSrc: './target/ps01001002_03.mind',
+      imageTargetSrc: '../assets/targets/musicband.mind',
     });
     const {renderer, scene, camera} = mindarThree;
 
     const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
     scene.add(light);
 
-    const model = await loadGLTF("./model/ps01001002_03.glb");
+    //const model = await loadGLTF("./model/ps01001002_03.glb");
+    const model = await loadGLTF("../assets/models/musicband-raccoon/scene.gltf");
     model.scene.scale.set(0.1, 0.1, 0.1);
     model.scene.position.set(0, -0.4, 0);
 
