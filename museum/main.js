4bffd39333f7d7bd1e2e4a8ef9d3c810119239a5
organguy <organguy@naver.com>
2022-02-22 14:16:47 +0900

    edt

--- museum/main.js
+++ museum/main.js
@@ -1,9 +1,11 @@
 import {loadGLTF} from "../libs/loader.js";
 const THREE = window.MINDAR.IMAGE.THREE;
 
-const scaleVal = 0.001;
+let STATE_TRACKING_START = 1;
+let STATE_TRACKING_FINISH = 2;
 
-var isInLoop = false;
+const scaleVal = 0.001;
+var isTrackingFound = false;
 
 document.addEventListener('DOMContentLoaded', () => {
 
@@ -17,61 +19,51 @@ document.addEventListener('DOMContentLoaded', () => {
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
       imageTargetSrc: targetPath,
-      //uiScanning: "no",
-      //uiLoading: "no",
+      uiScanning: "no",
+      uiLoading: "no",
     });
 
     const {renderer, scene, camera} = mindarThree;
-    console.log('mindarThree');
 
     const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
     scene.add(light);
 
-    console.log('light');
-
     const model = await loadGLTF(modelPath);
     model.scene.scale.set(scaleVal, scaleVal, scaleVal);
     model.scene.position.set(0, -0.4, 0);
-    console.log('model');
 
     const modelAnchor = mindarThree.addAnchor(0);
     modelAnchor.group.add(model.scene);
-    console.log('modelAnchor');
 
     modelAnchor.onTargetFound = () => {
-      console.log("on target found");
+      if(!isTrackingFound){
+          isTrackingFound = true;
+          Android.sendState(STATE_TRACKING_FINISH);
+      }
     }
     modelAnchor.onTargetLost = () => {
       console.log("on target lost");
     }
 
     const mixer = new THREE.AnimationMixer(model.scene);
-    console.log('mixer');
 
     const action = mixer.clipAction(model.animations[0]);
     action.setLoop(THREE.LoopOnce);
     //action.setLoop(THREE.LoopRepeat);
     action.play();
-    console.log('action');
 
     const clock = new THREE.Clock();
 
     await mindarThree.start();
-    console.log('mindarThree start');
+    Android.sendState(STATE_TRACKING_START);
 
     renderer.setAnimationLoop(() => {
-
-      if(!isInLoop){
-          console.log('in AnimationLoop');
-          isInLoop = true;
-      }
 
       const delta = clock.getDelta();
       mixer.update(delta);
 
       renderer.render(scene, camera);
     });
-    console.log('setAnimationLoop');
   }
 
   start();
