54c8b5373addde4ec957aa9fb299cd1bcb88debf
organguy <organguy@naver.com>
2022-02-22 13:38:12 +0900

    edt

--- museum/main.js
+++ museum/main.js
@@ -3,6 +3,8 @@ const THREE = window.MINDAR.IMAGE.THREE;
 
 const scaleVal = 0.001;
 
+var isInLoop = false;
+
 document.addEventListener('DOMContentLoaded', () => {
 
   const start = async() => {
@@ -18,34 +20,51 @@ document.addEventListener('DOMContentLoaded', () => {
       //uiScanning: "no",
       //uiLoading: "no",
     });
+
     const {renderer, scene, camera} = mindarThree;
+    console.log('mindarThree');
 
     const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
     scene.add(light);
 
+    console.log('light');
+
     const model = await loadGLTF(modelPath);
     model.scene.scale.set(scaleVal, scaleVal, scaleVal);
     model.scene.position.set(0, -0.4, 0);
+    console.log('model');
 
     const modelAnchor = mindarThree.addAnchor(0);
     modelAnchor.group.add(model.scene);
+    console.log('modelAnchor');
 
     const mixer = new THREE.AnimationMixer(model.scene);
+    console.log('mixer');
 
     const action = mixer.clipAction(model.animations[0]);
     action.setLoop(THREE.LoopOnce);
     //action.setLoop(THREE.LoopRepeat);
     action.play();
+    console.log('action');
 
     const clock = new THREE.Clock();
 
     await mindarThree.start();
+    console.log('mindarThree start');
+
     renderer.setAnimationLoop(() => {
+
+      if(!isInLoop){
+          console.log('in AnimationLoop');
+          isInLoop = true;
+      }
+
       const delta = clock.getDelta();
       mixer.update(delta);
 
       renderer.render(scene, camera);
     });
+    console.log('setAnimationLoop');
   }
 
   start();
