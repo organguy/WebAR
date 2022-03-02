d2b19f85102de56494f2c2e209eee638e238168d
organguy <organguy@naver.com>
2022-03-02 17:53:46 +0900

    edt

--- lectures-start/400-webxr-start/main.js
+++ lectures-start/400-webxr-start/main.js
@@ -2,6 +2,68 @@ import * as THREE from '../../libs/three.js-r132/build/three.module.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   const initialize = async() => {
+    const arButton = document.querySelector("#ar-button");
+
+    const supported = navigator.xr && await navigator.xr.isSessionSupported("immersive-ar");
+    if(!supported){
+      arButton.textContent = "Not Supported";
+      arButton.disabled = true;
+      return;
+    }
+
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera();
+    const renderer = new THREE.WebGLRenderer({
+      alpha: true,
+    });
+
+    renderer.setSize(window.innderWidth, window.innerHeight);
+    renderer.setPixelRatio(window.devicePixelRatio);
+    document.body.appendChild(renderer.domElement);
+
+    const geometry = new THREE.BoxGeometry(0.06, 0.06, 0.06);
+    const material = new THREE.MeshBasicMaterial({
+      color: 0x00ff00,
+    });
+    const mesh = new THREE.Mesh(geometry, material);
+    mesh.position(0, 0, -0.3);
+    scene.add(mesh);
+
+    const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
+    scene.add(light);
+
+    let currentSession = null;
+    const start = async() => {
+      currentSession = await navigator.xr.requestSession(
+        "immersive-ar",
+        {optionalFeatures: ['dom-overlay'], domOverlay: {root: document.body}}
+      );
+
+      renderer.xr.enabled = true;
+      renderer.xr.setReferenceSpaceType('local');
+      await renderer.xr.setSession(currentSession);
+
+      arButton.textContent = "End";
+
+      renderer.setAnimationLoop(() => {
+        renderer.render(scene, camera);
+      });
+    }
+
+    const end = async() => {
+      currentSession.end();
+      renderer.clear();
+      renderer.setAnimationLoop(null);
+    }
+
+    arButton.addEventListener("click", () => {
+      if(currentSession) {
+        end();
+      }else{
+        start();
+      }
+    });
+
   }
   initialize();
 });
